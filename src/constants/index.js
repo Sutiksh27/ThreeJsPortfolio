d98a81b7e26bd14b82e49a5f75483d6484bcb5c0
Sutiksh Verma <verma.su@northeastern.edu>
2023-07-21 22:39:56 -0700

    Changes in data format

--- src/constants/index.js
+++ src/constants/index.js
@@ -116,25 +116,16 @@ import {
   
   const experiences = [
     {
-      title: "Software Engineer Intern",
-      company_name: "Zypher",
-      icon: zypher,
-      iconBg: "#383E56",
-      date: "May 2019 - July 2019",
-      points: [
-        "Developed key features for the app, delivering them ahead of schedule, resulting in a 20% increase in user engagement.",
-        "Accelerated testing efficiency by automating unit tests, leading to a 60% time reduction and decreased reliance on manual intervention."
-      ],
-    },
-    {
-      title: "Software Engineer Intern",
-      company_name: "Sustanix",
-      icon: sustanix,
+      title: "Software Engineer Co-Op",
+      company_name: "Charles River Development",
+      icon: crd,
       iconBg: "#E6DEDD",
-      date: "Jan 2020 - April 2020",
+      date: "July 2022 - December 2022",
       points: [
-        "Developed and implemented advanced augmented reality (AR) models to showcase products, leading to a significant 30% increase in sales conversion for the company.",
-        "Enhanced user experience by incorporating personalized design elements including hover-over tips and intuitive animations, resulting in an average increase of 10 seconds spent on the product per user session."
+        "Implemented Gradle as the company's build automation tool, resulting in a 30% increase in build speed by converting XML scripts to Groovy scripts, leveraged online documentation to increase operating efficiency.",
+        "Successfully containerized company's projects in Docker, reducing install time by 50% on new releases; created small demos, and leveraged documentation Stack Overflow for guidance.",
+        "Optimized company's security, minimizing XXE & DTD attacks, reduced security breaches, identified affected files, applied code modifications to prevent parser vulnerabilities, collaborated with the security team.",
+        "Collaborated with a team of 50 SCRUM members to utilize agile methodologies by consistently achieving sprint targets with an average velocity of 3 story points.",
       ],
     },
     {
@@ -150,16 +141,25 @@ import {
       ],
     },
     {
-      title: "Software Engineer Co-Op",
-      company_name: "Charles River Development",
-      icon: crd,
+      title: "Software Engineer Intern",
+      company_name: "Sustanix",
+      icon: sustanix,
       iconBg: "#E6DEDD",
-      date: "July 2022 - December 2022",
+      date: "Jan 2020 - April 2020",
       points: [
-        "Implemented Gradle as the company's build automation tool, resulting in a 30% increase in build speed by converting XML scripts to Groovy scripts, leveraged online documentation to increase operating efficiency.",
-        "Successfully containerized company's projects in Docker, reducing install time by 50% on new releases; created small demos, and leveraged documentation Stack Overflow for guidance.",
-        "Optimized company's security, minimizing XXE & DTD attacks, reduced security breaches, identified affected files, applied code modifications to prevent parser vulnerabilities, collaborated with the security team.",
-        "Collaborated with a team of 50 SCRUM members to utilize agile methodologies by consistently achieving sprint targets with an average velocity of 3 story points.",
+        "Developed and implemented advanced augmented reality (AR) models to showcase products, leading to a significant 30% increase in sales conversion for the company.",
+        "Enhanced user experience by incorporating personalized design elements including hover-over tips and intuitive animations, resulting in an average increase of 10 seconds spent on the product per user session."
+      ],
+    },
+    {
+      title: "Software Engineer Intern",
+      company_name: "Zypher",
+      icon: zypher,
+      iconBg: "#383E56",
+      date: "May 2019 - July 2019",
+      points: [
+        "Developed key features for the app, delivering them ahead of schedule, resulting in a 20% increase in user engagement.",
+        "Accelerated testing efficiency by automating unit tests, leading to a 60% time reduction and decreased reliance on manual intervention."
       ],
     },
   ];
@@ -183,7 +183,7 @@ import {
       testimonial:
         "Sutiksh is a pleasure to work with. He is always responsive to our feedback and is always willing to help us understand the technical aspects of our projects.!",
       name: "Virender Singh",
-      designation: "SDE",
+      designation: "Data Scientist",
       company: "Salesforce",
     },
   ];
